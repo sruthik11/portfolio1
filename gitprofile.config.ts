545e30ce06d6b42d2b5d321871c33bea2937b6dd
Sruthi Sampath Kumar <39257137+sruthik11@users.noreply.github.com>
2024-07-20 08:20:12 -0700

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -87,7 +87,7 @@ const CONFIG = {
   },
   resume: {
     fileUrl:
-      'https://drive.google.com/file/d/166g9zVKZWlBLhg6fsdqDuabWrP8oE3l-/view?usp=sharing', // Empty fileUrl will hide the `Download Resume` button.
+      'https://drive.google.com/file/d/1J7bn11QPsRMPQ9iz53MWrJavS7FWruY2/view?usp=sharing', // Empty fileUrl will hide the `Download Resume` button.
   },
   skills: [
     'Python',
